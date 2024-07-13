4487941b9837f6356cf866fb756a10fb7e3ef847
AnthonyTB <anthonylol@protonmail.com>
2024-07-13 11:14:42 -0400

    fixed warnings

---
+++ pages/clients/_client.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mantine/core";
+
+export default function Client() {
+  return <Container>wip</Container>;
+}
--- pages/clients/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from "@mantine/core";
-
-export default function CaseStudies() {
-  return <Container></Container>;
-}
